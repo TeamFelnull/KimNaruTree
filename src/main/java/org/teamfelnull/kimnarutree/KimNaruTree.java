29d4f150293e89c0a14cf9180a2b94d0b0360aaa
MORIMORI0317 <43478594+MORIMORI0317@users.noreply.github.com>
2020-04-05 21:13:28 +0900

    frist

---
+++ src/main/java/org/teamfelnull/kimnarutree/KimNaruTree.java
@@ -0,0 +1,46 @@
+package org.teamfelnull.kimnarutree;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("kimnarutree")
+public class KimNaruTree {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public KimNaruTree() {
+
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+
+	}
+
+	private void doClientStuff(final FMLClientSetupEvent event) {
+
+	}
+
+	private void enqueueIMC(final InterModEnqueueEvent event) {
+
+	}
+
+	private void processIMC(final InterModProcessEvent event) {
+
+	}
+
+}
