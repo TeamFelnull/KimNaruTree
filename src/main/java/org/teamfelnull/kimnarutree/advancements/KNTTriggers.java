177b9b88ef2c188a13f9650b95fca7232168f466
nin8995 <sho.11.11.ggl@gmail.com>
2020-04-10 18:17:18 +0900

    進捗test

---
+++ src/main/java/org/teamfelnull/kimnarutree/advancements/KNTTriggers.java
@@ -0,0 +1,11 @@
+package org.teamfelnull.kimnarutree.advancements;
+
+import net.minecraft.advancements.CriteriaTriggers;
+
+public class KNTTriggers {
+
+	public static void registerTriggers() {
+		CriteriaTriggers.register(TestTrigger.INSTANCE);
+	}
+
+}
