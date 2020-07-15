1572d39774bb932fb9ef84dbd104697b0ab2c9f3
MORIMORI <morimori.0317@outlook.jp>
2020-07-15 23:44:55 +0900

    1.15.2 last

--- src/main/java/org/teamfelnull/kimnarutree/client/handler/ClientHandler.java
+++ src/main/java/org/teamfelnull/kimnarutree/client/handler/ClientHandler.java
@@ -1,100 +1,96 @@
 package org.teamfelnull.kimnarutree.client.handler;
 
-import org.teamfelnull.kimnarutree.client.gui.TestScreen;
-import org.teamfelnull.kimnarutree.handler.MessagePassbookHandler;
-import org.teamfelnull.kimnarutree.packet.MessageSendSysmtemInfo;
-import org.teamfelnull.kimnarutree.packet.PacketHandler;
-import org.teamfelnull.kimnarutree.util.MCHelper;
-import org.teamfelnull.kimnarutree.util.ModUtil;
-
 import com.mojang.blaze3d.platform.PlatformDescriptors;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import org.teamfelnull.kimnarutree.handler.MessagePassbookHandler;
+import org.teamfelnull.kimnarutree.packet.MessageSendSysmtemInfo;
+import org.teamfelnull.kimnarutree.packet.PacketHandler;
+import org.teamfelnull.kimnarutree.util.MCHelper;
+import org.teamfelnull.kimnarutree.util.ModUtil;
 import oshi.SystemInfo;
 import oshi.software.os.OperatingSystem;
 
 public class ClientHandler {
 
-	private static boolean frist;
-	public static String gupname;
-	public static Minecraft mc = Minecraft.getInstance();
+    private static boolean frist;
+    public static String gupname;
+    public static Minecraft mc = Minecraft.getInstance();
 
-	@SubscribeEvent
-	public static void onTick(TickEvent.ClientTickEvent e) {
+    @SubscribeEvent
+    public static void onTick(TickEvent.ClientTickEvent e) {
 
-		if (!frist) {
-			gupname = PlatformDescriptors.func_227776_c_();
-		}
-		frist = true;
+        if (!frist) {
+            gupname = PlatformDescriptors.func_227776_c_();
+        }
+        frist = true;
 
-		if (mc.player == null) {
-			return;
-		}
+        if (mc.player == null) {
+            return;
+        }
 
-		long i = Runtime.getRuntime().maxMemory();
-		long j = Runtime.getRuntime().totalMemory();
-		long k = Runtime.getRuntime().freeMemory();
-		long l = j - k;
+        long i = Runtime.getRuntime().maxMemory();
+        long j = Runtime.getRuntime().totalMemory();
+        long k = Runtime.getRuntime().freeMemory();
+        long l = j - k;
 
-		String memory = String.format("% 2d%% %03d/%03dMB", l * 100L / i, bytesToMb(l), bytesToMb(i));
-		@SuppressWarnings({ "resource", "static-access" })
-		String fps = MCHelper.getFPS() + "fps / " + mc.getInstance().gameSettings.framerateLimit + "fps";
-		PacketHandler.INSTANCE.sendToServer(new MessageSendSysmtemInfo("", "", "", "", "", memory, fps, true));
-	}
+        String memory = String.format("% 2d%% %03d/%03dMB", l * 100L / i, bytesToMb(l), bytesToMb(i));
+        @SuppressWarnings({"resource", "static-access"})
+        String fps = MCHelper.getFPS() + "fps / " + mc.getInstance().gameSettings.framerateLimit + "fps";
+        PacketHandler.INSTANCE.sendToServer(new MessageSendSysmtemInfo("", "", "", "", "", memory, fps, true));
+    }
 
-	private static long bytesToMb(long bytes) {
-		return bytes / 1024L / 1024L;
-	}
+    private static long bytesToMb(long bytes) {
+        return bytes / 1024L / 1024L;
+    }
 
-	@SubscribeEvent
-	@SuppressWarnings("resource")
-	public static void onPlayerJoin(EntityJoinWorldEvent e) {
+    @SubscribeEvent
+    @SuppressWarnings("resource")
+    public static void onPlayerJoin(EntityJoinWorldEvent e) {
 
-		if (!e.getWorld().isRemote || !(e.getEntity() instanceof ClientPlayerEntity)) {
-			return;
-		}
+        if (!e.getWorld().isRemote || !(e.getEntity() instanceof ClientPlayerEntity)) {
+            return;
+        }
 
-		SystemInfo syminfo = new SystemInfo();
-		OperatingSystem oss = syminfo.getOperatingSystem();
-		String java = "Java " + System.getProperty("java.version") + " " + (mc.isJava64bit() ? "64bit" : "32bit");
-		String os = oss.getManufacturer() + " " + System.getProperty("os.name") + " " + System.getProperty("os.version")
-				+ " " + System.getProperty("os.arch");
-		String cpu = PlatformDescriptors.func_227775_b_();
-		String gpu = gupname;
-		String mod = "";
+        SystemInfo syminfo = new SystemInfo();
+        OperatingSystem oss = syminfo.getOperatingSystem();
+        String java = "Java " + System.getProperty("java.version") + " " + (mc.isJava64bit() ? "64bit" : "32bit");
+        String os = oss.getManufacturer() + " " + System.getProperty("os.name") + " " + System.getProperty("os.version")
+                + " " + System.getProperty("os.arch");
+        String cpu = PlatformDescriptors.func_227775_b_();
+        String gpu = gupname;
+        String mod = "";
 
-		for (ModInfo mods : ModList.get().getMods()) {
-			mod += "[" + ModUtil.getModName(mods.getModId()) + " " + ModUtil.getModVersion(mods.getModId()) + "]" + " ";
-		}
+        for (ModInfo mods : ModList.get().getMods()) {
+            mod += "[" + ModUtil.getModName(mods.getModId()) + " " + ModUtil.getModVersion(mods.getModId()) + "]" + " ";
+        }
 
-		PacketHandler.INSTANCE.sendToServer(new MessageSendSysmtemInfo(java, os, cpu, gpu, mod, "", "", false));
-	}
+        PacketHandler.INSTANCE.sendToServer(new MessageSendSysmtemInfo(java, os, cpu, gpu, mod, "", "", false));
+    }
 
-	@SubscribeEvent
-	public static void onChatReceived(ClientChatReceivedEvent e) {
-		if (e.getType() == ChatType.SYSTEM) {
-			if (e.getMessage() instanceof TranslationTextComponent) {
-				TranslationTextComponent me = (TranslationTextComponent) e.getMessage();
-				if (me.getKey().contains("death") && !me.getKey().contains("rip")) {
-					e.setCanceled(true);
-				}
-			}
-			if (e.getMessage().getString().contains("DataList: ")) {
-				MessagePassbookHandler.oldChatComponent = e.getMessage();
-			}
-		}
-	}
-
+    @SubscribeEvent
+    public static void onChatReceived(ClientChatReceivedEvent e) {
+        if (e.getType() == ChatType.SYSTEM) {
+            if (e.getMessage() instanceof TranslationTextComponent) {
+                TranslationTextComponent me = (TranslationTextComponent) e.getMessage();
+                if (me.getKey().contains("death") && !me.getKey().contains("rip")) {
+                    e.setCanceled(true);
+                }
+            }
+            if (e.getMessage().getString().contains("DataList: ")) {
+                MessagePassbookHandler.oldChatComponent = e.getMessage();
+            }
+        }
+    }
+/*
 	@SubscribeEvent
 	public static void onKey(KeyInputEvent e) {
 
@@ -103,11 +99,12 @@ public class ClientHandler {
 		}
 
 	}
-	//	@SubscribeEvent
-	//	public static void onTooltip(ItemTooltipEvent e) {
-	//
-	//		e.getToolTip().add(new StringTextComponent(
-	//				"test=" + Evaluations.getArmorEvaluation(e.getItemStack())));
-	//
-	//	}
+	*/
+    //	@SubscribeEvent
+    //	public static void onTooltip(ItemTooltipEvent e) {
+    //
+    //		e.getToolTip().add(new StringTextComponent(
+    //				"test=" + Evaluations.getArmorEvaluation(e.getItemStack())));
+    //
+    //	}
 }
