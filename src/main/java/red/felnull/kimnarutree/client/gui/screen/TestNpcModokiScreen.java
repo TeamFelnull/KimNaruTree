cf00b380a34ed44c3df85cae71fef2eac805ec11
MORIMORI <morimori.0317@outlook.jp>
2020-07-19 00:25:10 +0900

    test button

--- src/main/java/red/felnull/kimnarutree/client/gui/screen/TestNpcModokiScreen.java
+++ src/main/java/red/felnull/kimnarutree/client/gui/screen/TestNpcModokiScreen.java
@@ -1,8 +1,13 @@
 package red.felnull.kimnarutree.client.gui.screen;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.inventory.ChestScreen;
+import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import red.felnull.kimnarutree.KimNaruTree;
 import red.felnull.kimnarutree.container.TestNpcModokiContainer;
 import red.felnull.otyacraftengine.client.gui.screen.AbstractIkisugiContainerScreen;
@@ -19,4 +24,12 @@ public class TestNpcModokiScreen extends AbstractIkisugiContainerScreen<TestNpcM
         return SB_GUI_TEXTURE;
     }
 
+    @Override
+    public void initByIKSG() {
+        super.initByIKSG();
+        this.addWidgetByIKSG(new Button(this.getWidthByIKSG() / 2 - 100, 24 * 2, 98, 20, new TranslationTextComponent("fml.menu.mods"), button -> {
+            Minecraft.getInstance().player.sendStatusMessage(new StringTextComponent("test"), false);
+        }));
+
+    }
 }
